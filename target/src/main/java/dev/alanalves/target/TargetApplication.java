4d7096e3e9ac6b8c9bb8e9e6222d6b669ff05611
Alan Alves <alan.alvess@outlook.com>
2024-09-15 01:34:46 -0300

    Iniciando o Projeto

---
+++ target/src/main/java/dev/alanalves/target/TargetApplication.java
@@ -0,0 +1,17 @@
+package dev.alanalves.target;
+
+import dev.alanalves.target.utils.Cores;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TargetApplication.class, args);
+    }
+
+
+
+
+}
